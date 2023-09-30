453d3e9bb641a4634c4a842e0d8ac1370389f6a5
Antonin <antonin.pire@gmail.com>
2023-10-01 01:31:23 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let loading = false;
+
+	async function handleClick() {
+		loading = true;
+		await fetch('http://localhost:5173/api/seed', {
+			method: 'POST'
+		});
+		loading = false;
+	}
+</script>
+
+<button disabled={loading} on:click={handleClick}>{!loading ? 'Seed' : 'Seeding...'}</button>
