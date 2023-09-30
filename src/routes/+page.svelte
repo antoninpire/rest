5ec309aff8897d73dff6312c76bf1d72e7556b88
Antonin <antonin.pire@gmail.com>
2023-10-01 01:45:01 +0200

    test running cloud endpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
 	let loading = false;
+	let url = '';
+	let response = '';
+	let method: 'GET' | 'POST' = 'GET';
 
 	async function handleClick() {
 		loading = true;
-		await fetch('http://localhost:5173/api/seed', {
-			method: 'POST'
+		const res = await fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({
+				email: 'tech@tcheker.fr',
+				password: 'demo'
+			}),
+			headers: {
+				'Content-type': 'application/json'
+			}
 		});
+		const json = await res.json();
+		response = JSON.stringify(json, null, 2);
 		loading = false;
 	}
 </script>
 
-<button disabled={loading} on:click={handleClick}>{!loading ? 'Seed' : 'Seeding...'}</button>
+<input type="text" bind:value={url} class="bg-black border-white border min-w-[450px]" />
+<select bind:value={method} class="text-white bg-black">
+	<option value="GET">GET</option>
+	<option value="POST">POST</option>
+</select>
+<button disabled={loading} on:click={handleClick}>{!loading ? 'Send' : 'Sending...'}</button>
+<br />
+<pre>{response}</pre>
+
+<!-- local to local WORKS -->
+<!-- cloud to local WORKS -->
+<!-- local to cloud WORKS -->
+<!-- cloud to cloud ??? -->
